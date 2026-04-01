cd7f86c5c3294ad5a6ca56b2e99c4cede2863116
Feziek <josephclarksabado575@gmail.com>
2026-04-01 14:20:50 +0800

    feat: add task persistence and refactor rendering logic

--- scripts/script.js
+++ scripts/script.js
@@ -1,42 +1,56 @@
 const addButton = document.querySelector('button');
 const text = document.querySelector('#input');
 const dateInput = document.querySelector('#date');
-
-const tasks = [];
+const tasksArr = JSON.parse(localStorage.getItem('taskItemArr')) || [];
 const todoList = document.querySelector('#todolist');
 
-addButton.addEventListener('click', () => {
-	renderTodo();
-});
+renderSavedTodo();
 
-function renderTodo() {
-	const dateValue = dateInput.value;
-	const value = text.value;
+addButton.addEventListener('click', addTask);
+
+function renderTodo(taskItemObject) {
 	const paragraph = document.createElement('p');
 	const deleteBtn = document.createElement('button');
 
-	if (!value.trim()) {
-		console.log('Please enter a task!');
-		return;
-	}
-
-	tasks.push({
-		name: value,
-		date: dateValue
-	});
-
-	const { name, date } = tasks[tasks.length - 1];
+	const { name, date } = taskItemObject;
 
 	paragraph.textContent = `${name} ${date}`;
 	deleteBtn.textContent = 'Delete';
 	deleteBtn.addEventListener('click', () => {
+		const item = tasksArr.indexOf(taskItemObject);
 		paragraph.remove();
 		deleteBtn.remove();
+		tasksArr.splice(item, 1);
+		localStorage.setItem('taskItemArr', JSON.stringify(tasksArr));
 	});
 
 	todoList.appendChild(paragraph);
 	todoList.appendChild(deleteBtn);
 
 	text.value = '';
-	dateValue.value = '';
+	dateInput.value = '';
+	text.focus();
+}
+
+function renderSavedTodo() {
+	tasksArr.forEach(savedTodo => {
+		renderTodo(savedTodo);
+	});
+}
+
+function addTask() {
+	const taskName = text.value;
+	const dueDate = dateInput.value;
+	if (!taskName.trim()) return;
+
+	const taskItem = {
+		name: taskName,
+		date: dueDate
+	};
+
+	tasksArr.push(taskItem);
+
+	localStorage.setItem('taskItemArr', JSON.stringify(tasksArr));
+
+	renderTodo(taskItem);
 }
